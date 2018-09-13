028eee678f6655fede553110663f78324e9e26d4
Anton-Cao <cao.anton@gmail.com>
2018-09-12 20:48:07 -0400

    Update API

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var updateFunFact = function() {
   var xmlHttp = new XMLHttpRequest(); // object to make GET request
   xmlHttp.onreadystatechange = function() { // function that is called once we get the response
-    document.getElementById('fun-fact').innerText = xmlHttp.responseText;
+    var response = xmlHttp.responseText.slice(1, -1);
+    document.getElementById('fun-fact').innerHTML = response;
   };
-  xmlHttp.open('GET', 'http://numbersapi.com/random/math', true);
+  xmlHttp.open('GET', 'https://ron-swanson-quotes.herokuapp.com/v2/quotes', true);
   xmlHttp.send(null);
 }
 
