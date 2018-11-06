954ac6728fcb696e0f209900804ba5468195661a
Anton-Cao <cao.anton@gmail.com>
2018-11-06 16:25:53 -0500

    Add comments to js

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var updateFunFact = function() {
   var xmlHttp = new XMLHttpRequest(); // object to make GET request
   xmlHttp.onreadystatechange = function() { // function that is called once we get the response
     var response = xmlHttp.responseText.slice(1, -1);
-    document.getElementById('fun-fact').innerHTML = response;
+    document.getElementById('fun-fact').innerHTML = response; // update the text of the fun-fact element
   };
   xmlHttp.open('GET', 'https://ron-swanson-quotes.herokuapp.com/v2/quotes', true);
   xmlHttp.send(null);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // we only want to add the event listener on the button once the button has loaded
   updateFunFact();
-  document.getElementById('button').addEventListener('click', function() {
+  document.getElementById('button').addEventListener('click', function() { // this function will be run whenever the button is clicked
     updateFunFact();
   });
 });
